f557f551f4ac0feff36e524bb70b0a08f1eabdc8
mrgabo <sagitbk.km@gmail.com>
2026-09-20 20:54:49 -0500

    chore(release): bump Brailux versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         applicationId = "com.neuronovaapps.brailux"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
